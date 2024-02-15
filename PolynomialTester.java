0703e1e5fcb8cc9ef44a906a31af98cbe435e82d
Ahmed Utku ÖZÜDOĞRU <ahmedutkuozudogru@gmail.com>
2024-02-15 12:42:34 +0300

    Tester class added

---
+++ PolynomialTester.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class PolynomialTester {
+ public static void main(String[] args) {
+   ArrayList<Polynomial> pols=new ArrayList<Polynomial>();
+   double[] array={1,2,3,4,5,6,7,8,9};
+    Polynomial pol1 = new Polynomial();
+    Polynomial pol2 = new Polynomial(5,5);
+    Polynomial pol3 = new Polynomial(array);
+    pols.add(pol1);
+    pols.add(pol2);
+    pols.add(pol3);
+
+   for(Polynomial pol:pols){
+      System.out.println("***********************************");
+      System.out.println(pol.getCoefficents(0));
+      System.out.println(pol.getDegree());
+      System.out.println(pol);
+      System.out.println(pol.eval(5));
+      System.out.println(pol.eval2(5));
+      System.out.println("***********************************");
+
+   }
+    
+ }    
+}
