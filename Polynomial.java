c2a953219b6149dad05084f82cb02d2960ebcc11
Ahmed Utku ÖZÜDOĞRU <ahmedutkuozudogru@gmail.com>
2024-02-26 18:25:57 +0300

    new methods added

--- Polynomial.java
+++ Polynomial.java
@@ -1,16 +1,15 @@
 public class Polynomial {
     private double[] coefficients;
 
-    //Constructer for zer polynomial
-    public Polynomial(){
-        this.coefficients=new double[]{0};
+    // Constructer for zer polynomial
+    public Polynomial() {
+        this.coefficients = new double[] { 0 };
     }
 
-    
-    //Constructor for polynomial of the form P(x) = cxd
-    public Polynomial(int d,double c){
-        this.coefficients=new double[d+1];
-        this.coefficients[d]=c;
+    // Constructor for polynomial of the form P(x) = cxd
+    public Polynomial(int d, double c) {
+        this.coefficients = new double[d + 1];
+        this.coefficients[d] = c;
 
     }
 
@@ -19,16 +18,17 @@ public class Polynomial {
         this.coefficients = coefficents;
     }
 
-
     public double getCoefficents(int d) {
 
         return this.coefficients[d];
     }
-    public int getDegree(){
 
-        return this.coefficients.length-1;
+    public int getDegree() {
+
+        return this.coefficients.length - 1;
     }
- // Method to convert polynomial to string
+
+    // Method to convert polynomial to string
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
@@ -36,8 +36,7 @@ public class Polynomial {
             if (coefficients[i] != 0) {
                 if (result.length() > 0) {
                     result.append(" ");
-                    if (coefficients[i] > 0)
-                    {
+                    if (coefficients[i] > 0) {
                         result.append("+ ");
                     }
 
@@ -49,24 +48,64 @@ public class Polynomial {
         }
         return result.toString();
     }
- // Method for evaluating the polynomial at given x
-    public double eval(double x){
-        double res=0;
-        for(int i=0;i<coefficients.length;i++){
-            res+=coefficients[i]*Math.pow(x, i);
+
+    // Method for evaluating the polynomial at given x
+    public double eval(double x) {
+        double res = 0;
+        for (int i = 0; i < coefficients.length; i++) {
+            res += coefficients[i] * Math.pow(x, i);
         }
 
         return res;
-    }   
+    }
+
     // Method for evaluating the polynomial at given x but using Horners Method
-    public double eval2(double x){
-        double res=coefficients[coefficients.length-1];
-        for(int i=coefficients.length-2;i>=0;i--){
-            res=res*x+coefficients[i];
+    public double eval2(double x) {
+        double res = coefficients[coefficients.length - 1];
+        for (int i = coefficients.length - 2; i >= 0; i--) {
+            res = res * x + coefficients[i];
         }
 
         return res;
-    }   
+    }
 
+    /*
+     * creates a new array by getting the max degree from both of them
+     * then iterates both by adding their coefficents
+     * 
+     */
+    public Polynomial add(Polynomial p2) {
+        int maxDegree = Math.max(this.getDegree(), p2.getDegree());
+        double[] resultCoefficients = new double[maxDegree + 1];
+
+        for (int i = 0; i <= maxDegree; i++) {
+            resultCoefficients[i] = this.getCoefficents(i) + p2.getCoefficents(i);
+        }
+
+        return new Polynomial(resultCoefficients);
+    }
+    /*
+     * Same as add method but substract
+     */
+    public Polynomial sub(Polynomial p2) {
+        int maxDegree = Math.max(this.getDegree(), p2.getDegree());
+        double[] resultCoefficients = new double[maxDegree + 1];
+
+        for (int i = 0; i <= maxDegree; i++) {
+            resultCoefficients[i] = this.getCoefficents(i) - p2.getCoefficents(i);
+        }
+        return new Polynomial(resultCoefficients);
+    }
+    public Polynomial mul(Polynomial p2) {
+        int resultDegree = this.getDegree() + p2.getDegree();
+        double[] resultCoefficients = new double[resultDegree + 1];
     
+        for (int i = 0; i <= this.getDegree(); i++) {
+            for (int j = 0; j <= p2.getDegree(); j++) {
+                resultCoefficients[i + j] += this.getCoefficents(i) * p2.getCoefficents(j);
+            }
+        }
+    
+        return new Polynomial(resultCoefficients);
+    }
 }
